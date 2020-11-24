61022d43c11e59b271fb3a45b7435bb8b40b48e5
MacSergey <macsergey@me.com>
2020-11-24 15:54:39 +0300

    move patcher to commons

---
+++ ModsCommon/Mod.cs
@@ -0,0 +1,84 @@
+﻿using ColossalFramework;
+using ColossalFramework.Globalization;
+using ICities;
+using ModsCommon.UI;
+using ModsCommon.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ModsCommon
+{
+    public abstract class BaseMod<ModType> : IUserMod
+        where ModType : BaseMod<ModType>
+    {
+        public static ModType Instance { get; protected set; }
+        public static Logger Logger => Instance.ModLogger;
+        public static Version Version => Instance.ModVersion;
+        public static List<Version> Versions => Instance.ModVersions;
+        public static bool IsBeta => Instance.ModIsBeta;
+        public static string ShortName => Instance.ModName;
+
+        public string Name => !ModIsBeta ? $"{ModName} {Version.GetString()}" : $"{ModName} {ModVersion.GetString()} [BETA]";
+        public string Description => ModDescription;
+
+        protected abstract string ModName { get; }
+        protected abstract string ModDescription { get; }
+
+
+        public abstract string WorkshopUrl { get; }
+        protected abstract Version ModVersion { get; }
+        protected abstract List<Version> ModVersions { get; }
+        public abstract string Id { get; }
+        protected abstract bool ModIsBeta { get; }
+        protected Logger ModLogger { get; private set; }
+
+        protected string ModLocale { get; }
+
+        protected CultureInfo Culture
+        {
+            get
+            {
+                var locale = string.IsNullOrEmpty(ModLocale) ? SingletonLite<LocaleManager>.instance.language : ModLocale;
+                if (locale == "zh")
+                    locale = "zh-cn";
+
+                return new CultureInfo(locale);
+            }
+        }
+
+        public BaseMod()
+        {
+            ModLogger = new Logger(Id);
+        }
+        public virtual void OnEnabled()
+        {
+            Instance = (ModType)this;
+            ModLogger.Debug($"Version {ModVersion}");
+            ModLogger.Debug($"Enabled");
+            LoadingManager.instance.m_introLoaded += LoadedError;
+        }
+        public virtual void OnDisabled()
+        {
+            ModLogger.Debug($"Disabled");
+            LoadingManager.instance.m_introLoaded -= LoadedError;
+            LocaleManager.eventLocaleChanged -= LocaleChanged;
+        }
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            LocaleManager.eventLocaleChanged -= LocaleChanged;
+            LocaleChanged();
+            LocaleManager.eventLocaleChanged += LocaleChanged;
+
+            ModLogger.Debug($"OnSettingsUI");
+            GetSettings(helper);
+        }
+        protected virtual void GetSettings(UIHelperBase helper) { }
+
+        public virtual void LocaleChanged() { }
+
+        public virtual void LoadedError() { }
+    }
+}
---
+++ ModsCommon/Patcher.cs
@@ -0,0 +1,67 @@
+﻿using CitiesHarmony.API;
+using HarmonyLib;
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ModsCommon
+{
+    public abstract class Patcher<ModType>
+        where ModType : BaseMod<ModType>
+    {
+        protected Harmony Harmony { get; } = new Harmony(BaseMod<ModType>.Instance.Id);
+        public bool Success { get; private set; }
+
+        public void Patch()
+        {
+            BaseMod<ModType>.Logger.Debug("Patch");
+            HarmonyHelper.DoOnHarmonyReady(() => Begin());
+        }
+        public void Unpatch()
+        {
+            BaseMod<ModType>.Logger.Debug($"Unpatch all");
+            Harmony.UnpatchAll();
+            BaseMod<ModType>.Logger.Debug($"Unpatched");
+        }
+
+        private void Begin()
+        {
+            BaseMod<ModType>.Logger.Debug("Start patching");
+            Success = PatchProcess();
+            BaseMod<ModType>.Logger.Debug(Success ? "Patch success" : "Patch Filed");
+        }
+        protected abstract bool PatchProcess();
+
+        protected bool AddPrefix(MethodInfo prefix, Type type, string method, Func<Type, string, MethodInfo> originalGetter = null)
+            => AddPatch((original) => Harmony.Patch(original, prefix: new HarmonyMethod(prefix)), type, method, originalGetter);
+
+        protected bool AddPostfix(MethodInfo postfix, Type type, string method, Func<Type, string, MethodInfo> originalGetter = null)
+            => AddPatch((original) => Harmony.Patch(original, postfix: new HarmonyMethod(postfix)), type, method, originalGetter);
+
+        protected bool AddTranspiler(MethodInfo transpiler, Type type, string method, Func<Type, string, MethodInfo> originalGetter = null)
+            => AddPatch((original) => Harmony.Patch(original, transpiler: new HarmonyMethod(transpiler)), type, method, originalGetter);
+
+        protected bool AddPatch(Action<MethodInfo> patch, Type type, string method, Func<Type, string, MethodInfo> originalGetter)
+        {
+            var methodName = $"{type.Name}.{method}()";
+            try
+            {
+                BaseMod<ModType>.Logger.Debug($"Patch {methodName}");
+
+                var original = originalGetter?.Invoke(type, method) ?? AccessTools.Method(type, method);
+                patch(original);
+
+                BaseMod<ModType>.Logger.Debug($"Patched {methodName}");
+                return true;
+            }
+            catch (Exception error)
+            {
+                BaseMod<ModType>.Logger.Error($"Failed Patch {methodName}", error);
+                return false;
+            }
+        }
+    }
+}
