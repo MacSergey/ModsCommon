d2f216c30353c64c37b747ba87be6344f2675b3f
MacSergey <macsergey@me.com>
2020-11-30 20:26:11 +0300

    CommonNetInfo

---
+++ ModsCommon/Utilities/CommonNetInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModsCommon.Utilities
+{
+    public class CommonNetInfo : NetInfo
+    {
+        protected HashSet<float> MarkingPointsRow {get; private set; } = new HashSet<float>();
+        protected void Clear() => MarkingPointsRow.Clear();
+        public float[] MarkupPoints => MarkingPointsRow.OrderBy(p => p).ToArray();
+        public bool HasMarkingPoints => MarkingPointsRow.Any();
+    }
+}
