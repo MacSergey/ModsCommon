71e2a6548894b301f11c19f33eb38af8cb86fdbd
MacSergey <macsergey@me.com>
2021-03-18 20:14:29 +0300

    added AdvancedScrollablePanel

--- ModsCommon/Properties/AssemblyInfo.cs
+++ ModsCommon/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.21076.1957.760")]
-[assembly: AssemblyFileVersion("1.21076.1957.759")]
+[assembly: AssemblyVersion("1.21077.2004.802")]
+[assembly: AssemblyFileVersion("1.21077.2004.801")]
--- ModsCommon/UI/Elements/AutoLayoutPanel.cs
+++ ModsCommon/UI/Elements/AutoLayoutPanel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace ModsCommon.UI
 {
@@ -29,5 +30,56 @@ namespace ModsCommon.UI
 
         public void StopLayout() => autoLayout = false;
         public void StartLayout() => autoLayout = true;
+    }
+    public class AdvancedScrollablePanel : UIPanel, IAutoLayoutPanel
+    {
+        public UIAutoLayoutScrollablePanel Content { get; private set; }
+        public AdvancedScrollablePanel()
+        {
+            AddPanel();
+        }
+
+        private void AddPanel()
+        {
+            Content = AddUIComponent<UIAutoLayoutScrollablePanel>();
+
+            Content.autoLayoutDirection = LayoutDirection.Vertical;
+            Content.scrollWheelDirection = UIOrientation.Vertical;
+            Content.builtinKeyNavigation = true;
+            Content.clipChildren = true;
+            Content.autoLayoutPadding = new RectOffset(0, 0, 0, 0);
+
+            this.AddScrollbar(Content);
+
+            Content.eventSizeChanged += ContentSizeChanged;
+            Content.verticalScrollbar.eventVisibilityChanged += ScrollbarVisibilityChanged;
+        }
+
+        private void ContentSizeChanged(UIComponent component, Vector2 value)
+        {
+            foreach (var item in Content.components)
+                item.width = Content.width - Content.autoLayoutPadding.horizontal;
+        }
+
+        private bool InProgress { get; set; } = false;
+        private void ScrollbarVisibilityChanged(UIComponent component, bool value)
+        {
+            if (InProgress && !value)
+                return;
+
+            InProgress = true;
+            SetContentSize();
+            InProgress = false;
+        }
+
+        protected override void OnSizeChanged()
+        {
+            base.OnSizeChanged();
+            SetContentSize();
+        }
+
+        private void SetContentSize() => Content.size = size - new Vector2(Content.verticalScrollbar.isVisible ? Content.verticalScrollbar.width :0, 0);
+        public void StopLayout() => Content.autoLayout = false;
+        public void StartLayout() => Content.autoLayout = true;
     }
 }
