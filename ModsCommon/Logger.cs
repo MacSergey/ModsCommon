48024815191346ac2f1291b43ec1b9eaa37afbdb
MacSergey <macsergey@me.com>
2021-03-19 15:44:54 +0300

    Update logger

--- ModsCommon/Logger.cs
+++ ModsCommon/Logger.cs
@@ -12,7 +12,9 @@ namespace ModsCommon
         public Logger(string name) => Name = name;
 
         public void Debug(string message) => Log(UnityEngine.Debug.Log, message);
-        public void Error(string message, Exception error = null) => Log(UnityEngine.Debug.LogError, error == null ? message : $"{message}\n{error.Message}\n{error.StackTrace}");
+        public void Warning(string message, Exception error = null) => Log(UnityEngine.Debug.LogWarning, GetMessage(message, error));
+        public void Error(string message, Exception error = null) => Log(UnityEngine.Debug.LogError, GetMessage(message, error));
         private void Log(Action<string> logFunc, string message) => logFunc($"[{Name}] {message}");
+        private string GetMessage(string message, Exception error) => error == null ? message : $"{message}\n{error.Message}\n{error.StackTrace}";
     }
 }
