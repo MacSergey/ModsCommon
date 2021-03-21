81a1839cb1ce3efe81bcf840e0df86956604af15
MacSergey <macsergey@me.com>
2021-03-21 21:04:40 +0300

    update EnumExtension

--- ModsCommon/Utilities/Extensions/EnumExtension.cs
+++ ModsCommon/Utilities/Extensions/EnumExtension.cs
@@ -7,16 +7,26 @@ namespace ModsCommon.Utilities
 {
     public static class EnumExtension
     {
-        public static AttrType GetAttr<AttrType, T>(this T value) where T : Enum where AttrType : Attribute
-            => typeof(T).GetField(value.ToString()).GetCustomAttributes(typeof(AttrType), false).OfType<AttrType>().FirstOrDefault();
-        public static IEnumerable<Type> GetEnumValues<Type>() where Type : Enum => Enum.GetValues(typeof(Type)).OfType<Type>().Where(v => v.IsVisible());
-        public static IEnumerable<Type> GetEnumValues<Type>(this Type value) where Type : Enum => GetEnumValues<Type>().Where(v => (value.ToInt() & v.ToInt()) != 0);
-        public static Type GetEnum<Type>(this List<Type> values) where Type : Enum => values.Aggregate(0, (r, v) => r | v.ToInt()).ToEnum<Type>();
+        public static AttrType GetAttr<AttrType, T>(this T value)
+            where T : Enum where AttrType : Attribute
+        {
+            return typeof(T).GetField(value.ToString()).GetCustomAttributes(typeof(AttrType), false).OfType<AttrType>().FirstOrDefault();
+        }
+        private static Func<T, bool> GetVisibleSelector<T>() where T : Enum => (value) => value.IsVisible();
+        public static IEnumerable<T> GetEnumValues<T>(Func<T, bool> selector = null)
+            where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).OfType<T>().Where(selector ?? GetVisibleSelector<T>());
+        }
+        public static IEnumerable<T> GetEnumValues<T>(this T value) where T : Enum => GetEnumValues<T>().Where(v => (value.ToInt() & v.ToInt()) != 0);
+        public static T GetEnum<T>(this List<T> values) where T : Enum => values.Aggregate(0, (r, v) => r | v.ToInt()).ToEnum<T>();
         public static bool IsVisible<T>(this T value) where T : Enum => value.GetAttr<NotVisibleAttribute, T>() == null;
+        public static bool IsItem<T>(this T value) where T : Enum => value.GetAttr<NotItemAttribute, T>() == null;
 
         public static int ToInt<T>(this T value) where T : Enum => (int)(object)value;
         public static T ToEnum<T>(this int value) where T : Enum => (T)(object)value;
     }
 
     public class NotVisibleAttribute : Attribute { }
+    public class NotItemAttribute : Attribute { }
 }
