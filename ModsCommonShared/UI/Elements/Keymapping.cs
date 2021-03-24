069af5791b92712b565aa96169ec515729abcddb
MacSergey <macsergey@me.com>
2021-03-24 19:01:42 +0300

    update keymapping

--- ModsCommonShared/UI/Elements/Keymapping.cs
+++ ModsCommonShared/UI/Elements/Keymapping.cs
@@ -1,5 +1,7 @@
 ﻿using ColossalFramework;
+using ColossalFramework.Globalization;
 using ColossalFramework.UI;
+using ModsCommon.Utilities;
 using ModsCommon.Utils;
 using UnityEngine;
 
@@ -7,110 +9,81 @@ namespace ModsCommon.UI
 {
     public class KeymappingsPanel : UICustomControl
     {
-        private static readonly string keyBindingTemplate = "KeyBindingTemplate";
-        private SavedInputKey m_EditingBinding;
+        private SavedInputKey EditBinding { get; set; }
         private int count;
 
         public void AddKeymapping(Shortcut shortcut)
         {
-            UIPanel uipanel = component.AttachUIComponent(UITemplateManager.GetAsGameObject(keyBindingTemplate)) as UIPanel;
+            var panel = component.AttachUIComponent(UITemplateManager.GetAsGameObject("KeyBindingTemplate")) as UIPanel;
 
-            int num = count;
-            count = num + 1;
-            if (num % 2 == 1)
-            {
-                uipanel.backgroundSprite = null;
-            }
+            if (count % 2 == 1)
+                panel.backgroundSprite = null;
 
-            UILabel uilabel = uipanel.Find<UILabel>("Name");
-            UIButton uibutton = uipanel.Find<UIButton>("Binding");
-            uibutton.eventKeyDown += OnBindingKeyDown;
-            uibutton.eventMouseDown += OnBindingMouseDown;
-            uilabel.text = shortcut.Label;
-            uibutton.text = shortcut.ToString();
-            uibutton.objectUserData = shortcut.InputKey;
+            count += 1;
+
+            var label = panel.Find<UILabel>("Name");
+            label.text = shortcut.Label;
+
+            var button = panel.Find<UIButton>("Binding");
+            button.eventKeyDown += OnBindingKeyDown;
+            button.eventMouseDown += OnBindingMouseDown;
+            button.text = shortcut.ToString();
+            button.objectUserData = shortcut.InputKey;
         }
-        private void OnLocaleChanged() => RefreshBindableInputs();
-        private bool IsModifierKey(KeyCode code) => code == KeyCode.LeftControl || code == KeyCode.RightControl || code == KeyCode.LeftShift || code == KeyCode.RightShift || code == KeyCode.LeftAlt || code == KeyCode.RightAlt;
-        private bool IsControlDown() => Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
-        private bool IsShiftDown() => Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        private bool IsAltDown() => Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt);
-        private bool IsUnbindableMouseButton(UIMouseButton code) => code == UIMouseButton.Left || code == UIMouseButton.Right;
-        private KeyCode ButtonToKeycode(UIMouseButton button)
-        {
-            return button switch
-            {
-                UIMouseButton.Left => KeyCode.Mouse0,
-                UIMouseButton.Right => KeyCode.Mouse1,
-                UIMouseButton.Middle => KeyCode.Mouse2,
-                UIMouseButton.Special0 => KeyCode.Mouse3,
-                UIMouseButton.Special1 => KeyCode.Mouse4,
-                UIMouseButton.Special2 => KeyCode.Mouse5,
-                UIMouseButton.Special3 => KeyCode.Mouse6,
-                _ => KeyCode.None,
-            };
-        }
+
         private void OnBindingKeyDown(UIComponent comp, UIKeyEventParameter p)
         {
-            if (m_EditingBinding != null && !IsModifierKey(p.keycode))
+            if (EditBinding != null && !IsModifierKey(p.keycode))
             {
                 p.Use();
                 UIView.PopModal();
-                KeyCode keycode = p.keycode;
-                InputKey value = (p.keycode == KeyCode.Escape) ? m_EditingBinding.value : SavedInputKey.Encode(keycode, p.control, p.shift, p.alt);
+
                 if (p.keycode == KeyCode.Backspace)
-                {
-                    value = SavedInputKey.Empty;
-                }
-                m_EditingBinding.value = value;
-                (p.source as UITextComponent).text = m_EditingBinding.ToLocalizedString("KEYNAME");
-                m_EditingBinding = null;
+                    EditBinding.value = SavedInputKey.Empty;
+                else if (p.keycode != KeyCode.Escape)
+                    EditBinding.value = SavedInputKey.Encode(p.keycode, p.control, p.shift, p.alt);
+
+                (p.source as UITextComponent).text = EditBinding.ToLocalizedString("KEYNAME");
+                EditBinding = null;
             }
         }
         private void OnBindingMouseDown(UIComponent comp, UIMouseEventParameter p)
         {
-            if (m_EditingBinding == null)
+            if (EditBinding == null)
             {
                 p.Use();
-                m_EditingBinding = (SavedInputKey)p.source.objectUserData;
-                UIButton uibutton = p.source as UIButton;
-                uibutton.buttonsMask = (UIMouseButton.Left | UIMouseButton.Right | UIMouseButton.Middle | UIMouseButton.Special0 | UIMouseButton.Special1 | UIMouseButton.Special2 | UIMouseButton.Special3);
-                uibutton.text = "Press any key";
+                EditBinding = (SavedInputKey)p.source.objectUserData;
+                var button = p.source as UIButton;
+                button.buttonsMask = UIMouseButton.Left | UIMouseButton.Right | UIMouseButton.Middle | UIMouseButton.Special0 | UIMouseButton.Special1 | UIMouseButton.Special2 | UIMouseButton.Special3;
+                button.text = Locale.Get("KEYMAPPING_PRESSANYKEY");
                 p.source.Focus();
                 UIView.PushModal(p.source);
-                return;
             }
-            if (!IsUnbindableMouseButton(p.buttons))
+            else if (!IsUnbindableMouseButton(p.buttons))
             {
                 p.Use();
                 UIView.PopModal();
-                InputKey value = SavedInputKey.Encode(ButtonToKeycode(p.buttons), IsControlDown(), IsShiftDown(), IsAltDown());
-                m_EditingBinding.value = value;
-                UIButton uibutton2 = p.source as UIButton;
-                uibutton2.text = m_EditingBinding.ToLocalizedString("KEYNAME");
-                uibutton2.buttonsMask = UIMouseButton.Left;
-                m_EditingBinding = null;
+                EditBinding.value = SavedInputKey.Encode(ButtonToKeycode(p.buttons), InputExtension.CtrlIsPressed, InputExtension.ShiftIsPressed, InputExtension.AltIsPressed);
+                var button = p.source as UIButton;
+                button.text = EditBinding.ToLocalizedString("KEYNAME");
+                button.buttonsMask = UIMouseButton.Left;
+                EditBinding = null;
             }
         }
-        private void RefreshBindableInputs()
+
+        private KeyCode ButtonToKeycode(UIMouseButton button) => button switch
         {
-            foreach (UIComponent uicomponent in base.component.GetComponentsInChildren<UIComponent>())
-            {
-                UITextComponent uitextComponent = uicomponent.Find<UITextComponent>("Binding");
-                if (uitextComponent != null)
-                {
-                    SavedInputKey savedInputKey = uitextComponent.objectUserData as SavedInputKey;
-                    if (savedInputKey != null)
-                    {
-                        uitextComponent.text = savedInputKey.ToLocalizedString("KEYNAME");
-                    }
-                }
-                UILabel uilabel = uicomponent.Find<UILabel>("Name");
-                if (uilabel != null)
-                {
-                    uilabel.text = uilabel.stringUserData;
-                }
-            }
-        }
+            UIMouseButton.Left => KeyCode.Mouse0,
+            UIMouseButton.Right => KeyCode.Mouse1,
+            UIMouseButton.Middle => KeyCode.Mouse2,
+            UIMouseButton.Special0 => KeyCode.Mouse3,
+            UIMouseButton.Special1 => KeyCode.Mouse4,
+            UIMouseButton.Special2 => KeyCode.Mouse5,
+            UIMouseButton.Special3 => KeyCode.Mouse6,
+            _ => KeyCode.None,
+        };
+
+        private bool IsModifierKey(KeyCode code) => code == KeyCode.LeftControl || code == KeyCode.RightControl || code == KeyCode.LeftShift || code == KeyCode.RightShift || code == KeyCode.LeftAlt || code == KeyCode.RightAlt;
+        private bool IsUnbindableMouseButton(UIMouseButton code) => code == UIMouseButton.Left || code == UIMouseButton.Right;
     }
 }
