759e49a84a6901ce799e676bc1dcd6415c25184e
MacSergey <macsergey@me.com>
2021-04-21 11:10:03 +0300

    added base serializable data extension

---
+++ ModsCommonShared/Utilities/Loader.cs
@@ -0,0 +1,56 @@
+﻿using ColossalFramework.Globalization;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ModsCommon.Utilities
+{
+    public abstract class Loader
+    {
+        public static string GetString(XElement config) => config.ToString(SaveOptions.DisableFormatting);
+
+        public static byte[] Compress(string xml)
+        {
+            var buffer = Encoding.UTF8.GetBytes(xml);
+
+            using var outStream = new MemoryStream();
+            using (var zipStream = new GZipStream(outStream, CompressionMode.Compress))
+            {
+                zipStream.Write(buffer, 0, buffer.Length);
+            }
+            var compresed = outStream.ToArray();
+            return compresed;
+        }
+
+        public static string Decompress(byte[] data)
+        {
+            using var inStream = new MemoryStream(data);
+            using var zipStream = new GZipStream(inStream, CompressionMode.Decompress);
+            using var outStream = new MemoryStream();
+            byte[] buffer = new byte[1000000];
+            int readed;
+            while ((readed = zipStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                outStream.Write(buffer, 0, readed);
+            }
+
+            var decompressed = outStream.ToArray();
+            var xml = Encoding.UTF8.GetString(decompressed);
+            return xml;
+        }
+
+        public static string GetSaveName()
+        {
+            var lastSaveField = AccessTools.Field(typeof(SavePanel), "m_LastSaveName");
+            var lastSave = lastSaveField.GetValue(null) as string;
+            if (string.IsNullOrEmpty(lastSave))
+                lastSave = Locale.Get("DEFAULTSAVENAMES", "NewSave");
+
+            return lastSave;
+        }
+    }
+}
---
+++ ModsCommonShared/Utilities/SerializableDataExtension.cs
@@ -0,0 +1,81 @@
+﻿using ICities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ModsCommon.Utilities
+{
+    public abstract class BaseSerializableDataExtension<TypeExtension, TypeMod> : SerializableDataExtensionBase
+        where TypeExtension : BaseSerializableDataExtension<TypeExtension, TypeMod>
+        where TypeMod : BaseMod<TypeMod>
+    {
+        protected abstract string Id { get; }
+        protected abstract XElement GetSaveData();
+        protected abstract void SetLoadData(XElement config);
+        protected virtual void OnLoadFailed() { }
+        protected virtual void OnSaveFailed(string config) { }
+
+        public override void OnCreated(ISerializableData serializableData)
+        {
+            base.OnCreated(serializableData);
+            SingletonItem<TypeExtension>.Instance = (TypeExtension)this;
+        }
+
+        public override void OnLoadData()
+        {
+            SingletonMod<TypeMod>.Logger.Debug($"Start load map data");
+
+            if (serializableDataManager.LoadData(Id) is byte[] data)
+            {
+                try
+                {
+                    var sw = Stopwatch.StartNew();
+
+                    var decompress = Loader.Decompress(data);
+#if DEBUG
+                    SingletonMod<TypeMod>.Logger.Debug(decompress);
+#endif
+                    var config = XmlExtension.Parse(decompress);
+                    SetLoadData(config);
+
+                    sw.Stop();
+                    SingletonMod<TypeMod>.Logger.Debug($"Map data was loaded in {sw.ElapsedMilliseconds}ms; Size = {data.Length} bytes");
+                }
+                catch (Exception error)
+                {
+                    SingletonMod<TypeMod>.Logger.Error("Could not load map data", error);
+                    OnLoadFailed();
+                }
+            }
+            else
+                SingletonMod<TypeMod>.Logger.Debug($"Saved map data not founded");
+        }
+        public override void OnSaveData()
+        {
+            SingletonMod<TypeMod>.Logger.Debug($"Start save map data");
+
+            var config = string.Empty;
+            try
+            {
+                var sw = Stopwatch.StartNew();
+                config = Loader.GetString(GetSaveData());
+#if DEBUG
+                SingletonMod<TypeMod>.Logger.Debug(config);
+#endif
+                var compress = Loader.Compress(config);
+                serializableDataManager.SaveData(Id, compress);
+
+                sw.Stop();
+                SingletonMod<TypeMod>.Logger.Debug($"Map data saved in {sw.ElapsedMilliseconds}ms; Size = {compress.Length} bytes");
+            }
+            catch (Exception error)
+            {
+                SingletonMod<TypeMod>.Logger.Error("Save map data failed", error);
+                OnSaveFailed(config);
+                throw;
+            }
+        }
+    }
+}
