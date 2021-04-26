6a9929ed3ad33c07a9853509151245685be1932b
MacSergey <macsergey@me.com>
2021-04-26 18:10:19 +0300

    added object map

---
+++ ModsCommonShared/Utilities/ObjectMap.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModsCommon.Utilities
+{
+    public abstract class BaseObjectsMap<TypeObjectId> : IEnumerable<KeyValuePair<TypeObjectId, TypeObjectId>>
+        where TypeObjectId : ObjectId, new()
+    {
+        public bool IsSimple { get; }
+        public bool IsEmpty => !Map.Any();
+        protected Dictionary<TypeObjectId, TypeObjectId> Map { get; } = new Dictionary<TypeObjectId, TypeObjectId>();
+
+        public TypeObjectId this[TypeObjectId key]
+        {
+            get => Map[key];
+            protected set
+            {
+                if (key == value)
+                    return;
+
+                Map[key] = value;
+            }
+        }
+
+        public BaseObjectsMap(bool isSimple = false)
+        {
+            IsSimple = isSimple;
+        }
+
+        public bool TryGetValue(TypeObjectId key, out TypeObjectId value) => Map.TryGetValue(key, out value);
+        public bool TryGetNode(ushort nodeIdKey, out ushort nodeIdValue)
+        {
+            if (Map.TryGetValue(new TypeObjectId() { Node = nodeIdKey }, out TypeObjectId value))
+            {
+                nodeIdValue = value.Node;
+                return true;
+            }
+            else
+            {
+                nodeIdValue = default;
+                return false;
+            }
+        }
+        public bool TryGetSegment(ushort segmentIdKey, out ushort segmentIdValue)
+        {
+            if (Map.TryGetValue(new TypeObjectId() { Segment = segmentIdKey }, out TypeObjectId value))
+            {
+                segmentIdValue = value.Node;
+                return true;
+            }
+            else
+            {
+                segmentIdValue = default;
+                return false;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TypeObjectId, TypeObjectId>> GetEnumerator() => Map.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+        public void AddSegment(ushort source, ushort target) => this[new TypeObjectId() { Segment = source }] = new TypeObjectId() { Segment = target };
+        public void AddNode(ushort source, ushort target) => this[new TypeObjectId() { Node = source }] = new TypeObjectId() { Node = target };
+
+        public void Remove(TypeObjectId key) => Map.Remove(key);
+
+        public delegate bool TryGetDelegate<T>(T key, out T value);
+    }
+    public class ObjectMap : BaseObjectsMap<ObjectId>
+    {
+        public ObjectMap(bool isSimple = false) : base(isSimple) { }
+    }
+
+    public class ObjectId
+    {
+        public static long DataMask = 0xFFFFFFFFL;
+        public static long TypeMask = DataMask << 32;
+        public static long NodeType = 1L << 32;
+        public static long SegmentType = 2L << 32;
+
+        public long Id;
+
+        public ushort Node
+        {
+            get => (Id & NodeType) == 0 ? 0 : (ushort)(Id & DataMask);
+            set => Id = NodeType | value;
+        }
+        public ushort Segment
+        {
+            get => (Id & SegmentType) == 0 ? 0 : (ushort)(Id & DataMask);
+            set => Id = SegmentType | value;
+        }
+        public long Type => Id & TypeMask;
+
+        public static bool operator ==(ObjectId x, ObjectId y) => x.Id == y.Id;
+        public static bool operator !=(ObjectId x, ObjectId y) => x.Id != y.Id;
+
+        public override bool Equals(object obj) => obj is ObjectId objectId && objectId == this;
+        public override int GetHashCode() => Id.GetHashCode();
+        public override string ToString()
+        {
+            if (Type == NodeType)
+                return $"{Type}: {Node}";
+            else if (Type == SegmentType)
+                return $"{Type}: {Segment}";
+            else
+                return $"{Type}: {Id}";
+        }
+    }
+}
