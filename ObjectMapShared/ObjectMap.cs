9e90c50aa518ff522418dd23c80c52980793b6db
MacSergey <macsergey@me.com>
2021-06-07 17:07:12 +0300

    added net object map

--- ObjectMapShared/ObjectMap.cs
+++ ObjectMapShared/ObjectMap.cs
@@ -6,11 +6,6 @@ using System.Text;
 
 namespace ModsCommon.Utilities
 {
-    public interface INetObjectsMap
-    {
-        void AddSegment(ushort source, ushort target);
-        void AddNode(ushort source, ushort target);
-    }
     public abstract class BaseObjectsMap<TypeObjectId> : IEnumerable<KeyValuePair<TypeObjectId, TypeObjectId>>
         where TypeObjectId : ObjectId, new()
     {
@@ -46,57 +41,6 @@ namespace ModsCommon.Utilities
 
         public delegate bool TryGetDelegate<T>(T key, out T value);
     }
-    public abstract class NetObjectsMap<TypeObjectId> : BaseObjectsMap<TypeObjectId>, INetObjectsMap
-        where TypeObjectId : NetObjectId, new()
-    {
-        public NetObjectsMap(bool isSimple = false) : base(isSimple) { }
-
-        public bool TryGetNode(ushort nodeIdKey, out ushort nodeIdValue)
-        {
-            if (Map.TryGetValue(new TypeObjectId() { Node = nodeIdKey }, out TypeObjectId value))
-            {
-                nodeIdValue = value.Node;
-                return true;
-            }
-            else
-            {
-                nodeIdValue = default;
-                return false;
-            }
-        }
-        public bool TryGetSegment(ushort segmentIdKey, out ushort segmentIdValue)
-        {
-            if (Map.TryGetValue(new TypeObjectId() { Segment = segmentIdKey }, out TypeObjectId value))
-            {
-                segmentIdValue = value.Segment;
-                return true;
-            }
-            else
-            {
-                segmentIdValue = default;
-                return false;
-            }
-        }
-       
-        public void AddSegment(ushort source, ushort target) => this[new TypeObjectId() { Segment = source }] = new TypeObjectId() { Segment = target };
-        public void AddNode(ushort source, ushort target) => this[new TypeObjectId() { Node = source }] = new TypeObjectId() { Node = target };
-
-        public void FromDictionary(Dictionary<InstanceID, InstanceID> sourceMap)
-        {
-            foreach (var source in sourceMap)
-            {
-                switch (source.Key.Type)
-                {
-                    case InstanceType.NetNode when source.Value.Type == InstanceType.NetNode:
-                        AddNode(source.Key.NetNode, source.Value.NetNode);
-                        break;
-                    case InstanceType.NetSegment when source.Value.Type == InstanceType.NetSegment:
-                        AddSegment(source.Key.NetSegment, source.Value.NetSegment);
-                        break;
-                }
-            }
-        }
-    }
 
     public class ObjectId
     {
@@ -123,31 +67,5 @@ namespace ModsCommon.Utilities
 
         public override int GetHashCode() => Id.GetHashCode();
         public override string ToString() => $"{Type}: {Id}";
-    }
-    public class NetObjectId : ObjectId
-    {
-        public static long NodeType = 1L << 32;
-        public static long SegmentType = 2L << 32;
-
-        public ushort Node
-        {
-            get => (Id & NodeType) == 0 ? 0 : (ushort)(Id & DataMask);
-            set => Id = NodeType | value;
-        }
-        public ushort Segment
-        {
-            get => (Id & SegmentType) == 0 ? 0 : (ushort)(Id & DataMask);
-            set => Id = SegmentType | value;
-        }
-
-        public override string ToString()
-        {
-            if (Type == NodeType)
-                return $"{nameof(Node)}: {Node}";
-            else if (Type == SegmentType)
-                return $"{nameof(Segment)}: {Segment}";
-            else
-                return base.ToString();
-        }
     }
 }
