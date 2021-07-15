c8c9dd247420a7c1126d44268ec2e28a7a67d12a
MacSergey <macsergey@me.com>
2021-07-15 18:44:01 +0300

    update UUI register

--- UUIRegisterShared/UUIRegister.cs
+++ UUIRegisterShared/UUIRegister.cs
@@ -12,7 +12,7 @@ namespace ModsCommon
     where TypeMod : ICustomMod
     where TypeTool : ToolBase, ITool
     {
-        private static PluginSearcher UUISearcher { get; } = PluginUtilities.GetSearcher("Unified UI", 2255219025ul);
+        private static PluginSearcher UUISearcher { get; } = PluginUtilities.GetSearcher("Unified UI", 2255219025ul) & new VersionSearcher(new Version(1,2),(m,s) => m >= s);
         public static bool IsUUIInstaled => UUISearcher.GetPlugin() != null;
 
         protected abstract UITextureAtlas UUIAtlas { get; }
