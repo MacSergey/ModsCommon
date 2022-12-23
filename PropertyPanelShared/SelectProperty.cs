f0a848bf8cf1d171e3b419f559b03408e00e2dfb
MacSergey <macsergey@me.com>
2022-12-23 05:41:08 +0300

    Updated select property

--- PropertyPanelShared/SelectProperty.cs
+++ PropertyPanelShared/SelectProperty.cs
@@ -3,11 +3,12 @@ using ModsCommon.Utilities;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using static NetInfo;
 
 namespace ModsCommon.UI
 {
     public abstract class SelectPropertyPanel<Type, PanelType> : EditorPropertyPanel, IReusable
-        where PanelType : SelectPropertyPanel<Type, PanelType>
+        where PanelType : SelectPropertyButton<Type>
     {
         public event Action<Type> OnValueChanged;
         public event Action<PanelType> OnSelect;
@@ -15,12 +16,14 @@ namespace ModsCommon.UI
         public event Action<PanelType> OnLeave;
 
         bool IReusable.InCache { get; set; }
-        protected abstract string NotSet { get; }
-        protected SelectPropertyButton Selector { get; set; }
-        protected CustomUIButton Button { get; set; }
+        public PanelType Selector { get; private set; }
         protected abstract float Width { get; }
 
-        public abstract Type Value { get; set; }
+        public Type Value
+        {
+            get => Selector.Value;
+            set => Selector.Value = value;
+        }
 
         public SelectPropertyPanel()
         {
@@ -28,43 +31,13 @@ namespace ModsCommon.UI
         }
         private void AddSelector()
         {
-            Selector = Content.AddUIComponent<SelectPropertyButton>();
-            Selector.text = NotSet;
-            Selector.atlas = CommonTextures.Atlas;
-            Selector.normalBgSprite = CommonTextures.FieldNormal;
-            Selector.hoveredBgSprite = CommonTextures.FieldHovered;
-            Selector.disabledBgSprite = CommonTextures.FieldDisabled;
-            Selector.focusedBgSprite = CommonTextures.FieldFocused;
-            Selector.isInteractive = false;
-            Selector.enabled = true;
-            Selector.autoSize = false;
-            Selector.textHorizontalAlignment = UIHorizontalAlignment.Left;
-            Selector.textVerticalAlignment = UIVerticalAlignment.Middle;
-            Selector.height = 20;
+            Selector = Content.AddUIComponent<PanelType>();
             Selector.width = Width;
-            Selector.textScale = 0.6f;
-            Selector.textPadding = new RectOffset(8, 0, 4, 0);
+            Selector.OnValueChanged += ValueChanged;
 
-            Button = Selector.AddUIComponent<CustomUIButton>();
-            Button.atlas = TextureHelper.InGameAtlas;
-            Button.text = string.Empty;
-            Button.size = Selector.size;
-            Button.relativePosition = new Vector3(0f, 0f);
-            Button.textVerticalAlignment = UIVerticalAlignment.Middle;
-            Button.textHorizontalAlignment = UIHorizontalAlignment.Left;
-            Button.normalFgSprite = "IconDownArrow";
-            Button.hoveredFgSprite = "IconDownArrowHovered";
-            Button.pressedFgSprite = "IconDownArrowPressed";
-            Button.focusedFgSprite = "IconDownArrow";
-            Button.foregroundSpriteMode = UIForegroundSpriteMode.Scale;
-            Button.horizontalAlignment = UIHorizontalAlignment.Right;
-            Button.verticalAlignment = UIVerticalAlignment.Middle;
-            Button.textScale = 0.8f;
-
-            Button.eventClick += ButtonClick;
-            Button.eventMouseEnter += ButtonMouseEnter;
-            Button.eventMouseLeave += ButtonMouseLeave;
-            Button.eventIsEnabledChanged += ButtonIsEnabledChanged;
+            Selector.Button.eventClick += ButtonClick;
+            Selector.Button.eventMouseEnter += ButtonMouseEnter;
+            Selector.Button.eventMouseLeave += ButtonMouseLeave;
         }
 
         public override void DeInit()
@@ -78,69 +51,19 @@ namespace ModsCommon.UI
         }
         public bool Selected
         {
-            get => Selector.state == UIButton.ButtonState.Focused;
-            set => Selector.state = value ? UIButton.ButtonState.Focused : UIButton.ButtonState.Normal;
+            get => Selector.Selected;
+            set => Selector.Selected = value;
         }
-        public new void Focus() => Button.Focus();
+        public new void Focus() => Selector.Focus();
 
-        private void ButtonIsEnabledChanged(UIComponent component, bool value) => Button.normalFgSprite = value ? "IconDownArrow" : "Empty";
-
-        protected void ValueChanged()
-        {
-            OnValueChanged?.Invoke(Value);
-            Selector.text = Value?.ToString() ?? NotSet;
-        }
-        protected virtual void ButtonClick(UIComponent component, UIMouseEventParameter eventParam) => OnSelect?.Invoke((PanelType)this);
-        protected virtual void ButtonMouseEnter(UIComponent component, UIMouseEventParameter eventParam) => OnEnter?.Invoke((PanelType)this);
-        protected virtual void ButtonMouseLeave(UIComponent component, UIMouseEventParameter eventParam) => OnLeave?.Invoke((PanelType)this);
+        protected void ValueChanged(Type value) => OnValueChanged?.Invoke(value);
+        protected virtual void ButtonClick(UIComponent component, UIMouseEventParameter eventParam) => OnSelect?.Invoke(component.parent as PanelType);
+        protected virtual void ButtonMouseEnter(UIComponent component, UIMouseEventParameter eventParam) => OnEnter?.Invoke(component.parent as PanelType);
+        protected virtual void ButtonMouseLeave(UIComponent component, UIMouseEventParameter eventParam) => OnLeave?.Invoke(component.parent as PanelType);
     }
 
-    public abstract class SelectListPropertyPanel<Type, PanelType> : SelectPropertyPanel<Type, PanelType>, IReusable
-        where PanelType : SelectListPropertyPanel<Type, PanelType>
-    {
-        private int SelectIndex { get; set; } = -1;
-
-        private List<Type> ObjectsList { get; set; } = new List<Type>();
-        public IEnumerable<Type> Objects => ObjectsList;
-        public override Type Value
-        {
-            get => SelectIndex == -1 ? default : ObjectsList[SelectIndex];
-            set => SetSelected(ObjectsList.FindIndex(o => IsEqual(value, o)));
-        }
-        private void SetSelected(int index)
-        {
-            if (index != SelectIndex)
-            {
-                SelectIndex = index;
-                ValueChanged();
-            }
-        }
-        public void Add(Type item) => ObjectsList.Add(item);
-        public void AddRange(IEnumerable<Type> items) => ObjectsList.AddRange(items);
-        public void Clear()
-        {
-            ObjectsList.Clear();
-            Value = default;
-        }
-
-        protected abstract bool IsEqual(Type first, Type second);
-    }
-    public abstract class SelectItemPropertyPanel<Type, PanelType> : SelectPropertyPanel<Type, PanelType>, IReusable
-        where PanelType : SelectItemPropertyPanel<Type, PanelType>
-    {
-        private Type _value;
-        public override Type Value
-        {
-            get => _value;
-            set
-            {
-                _value = value;
-                ValueChanged();
-            }
-        }
-    }
-    public abstract class ResetableSelectPropertyPanel<Type, PanelType> : SelectListPropertyPanel<Type, PanelType>
-                where PanelType : ResetableSelectPropertyPanel<Type, PanelType>
+    public abstract class ResetableSelectPropertyPanel<Type, PanelType> : SelectPropertyPanel<Type, PanelType>
+        where PanelType : SelectListPropertyButton<Type>
     {
         public event Action<PanelType> OnReset;
         protected abstract string ResetToolTip { get; }
@@ -171,11 +94,115 @@ namespace ModsCommon.UI
         protected virtual void ResetClick(UIComponent component, UIMouseEventParameter eventParam)
         {
             Value = default;
-            OnReset?.Invoke((PanelType)this);
+            OnReset?.Invoke(component as PanelType);
         }
     }
-    public class SelectPropertyButton : CustomUIButton
+    public abstract class SelectPropertyButton<Type> : CustomUIButton
     {
         public override bool containsFocus => false;
+        public CustomUIButton Button { get; }
+        protected abstract string NotSet { get; }
+
+
+        public event Action<Type> OnValueChanged;
+        public abstract Type Value { get; set; }
+
+        public SelectPropertyButton()
+        {
+            text = NotSet;
+            atlas = CommonTextures.Atlas;
+            normalBgSprite = CommonTextures.FieldNormal;
+            hoveredBgSprite = CommonTextures.FieldHovered;
+            disabledBgSprite = CommonTextures.FieldDisabled;
+            focusedBgSprite = CommonTextures.FieldFocused;
+            isInteractive = false;
+            enabled = true;
+            autoSize = false;
+            textHorizontalAlignment = UIHorizontalAlignment.Left;
+            textVerticalAlignment = UIVerticalAlignment.Middle;
+            height = 20;
+            textScale = 0.6f;
+            textPadding = new RectOffset(8, 0, 4, 0);
+
+            Button = AddUIComponent<CustomUIButton>();
+            Button.atlas = TextureHelper.InGameAtlas;
+            Button.text = string.Empty;
+            Button.size = size;
+            Button.relativePosition = new Vector3(0f, 0f);
+            Button.textVerticalAlignment = UIVerticalAlignment.Middle;
+            Button.textHorizontalAlignment = UIHorizontalAlignment.Left;
+            Button.normalFgSprite = "IconDownArrow";
+            Button.hoveredFgSprite = "IconDownArrowHovered";
+            Button.pressedFgSprite = "IconDownArrowPressed";
+            Button.focusedFgSprite = "IconDownArrow";
+            Button.foregroundSpriteMode = UIForegroundSpriteMode.Scale;
+            Button.horizontalAlignment = UIHorizontalAlignment.Right;
+            Button.verticalAlignment = UIVerticalAlignment.Middle;
+            Button.textScale = 0.8f;
+            Button.eventIsEnabledChanged += ButtonIsEnabledChanged;
+        }
+
+        protected override void OnSizeChanged()
+        {
+            base.OnSizeChanged();
+            if (Button != null)
+                Button.size = size;
+        }
+        private void ButtonIsEnabledChanged(UIComponent component, bool value) => Button.normalFgSprite = value ? "IconDownArrow" : "Empty";
+
+        protected void ValueChanged()
+        {
+            OnValueChanged?.Invoke(Value);
+            text = Value?.ToString() ?? NotSet;
+        }
+        public new void Focus() => Button.Focus();
+        public bool Selected
+        {
+            get => state == ButtonState.Focused;
+            set => state = value ? ButtonState.Focused : ButtonState.Normal;
+        }
+    }
+    public abstract class SelectListPropertyButton<Type> : SelectPropertyButton<Type>
+    {
+        private int SelectIndex { get; set; } = -1;
+
+        private List<Type> ObjectsList { get; set; } = new List<Type>();
+        public IEnumerable<Type> Objects => ObjectsList;
+        public override Type Value
+        {
+            get => SelectIndex == -1 ? default : ObjectsList[SelectIndex];
+            set => SetSelected(ObjectsList.FindIndex(o => IsEqual(value, o)));
+        }
+        private void SetSelected(int index)
+        {
+            if (index != SelectIndex)
+            {
+                SelectIndex = index;
+                ValueChanged();
+            }
+        }
+
+        public void Add(Type item) => ObjectsList.Add(item);
+        public void AddRange(IEnumerable<Type> items) => ObjectsList.AddRange(items);
+        public void Clear()
+        {
+            ObjectsList.Clear();
+            Value = default;
+        }
+
+        protected abstract bool IsEqual(Type first, Type second);
+    }
+    public abstract class SelectItemPropertyButton<Type> : SelectPropertyButton<Type>
+    {
+        private Type _value;
+        public override Type Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                ValueChanged();
+            }
+        }
     }
 }
