f1d64bf3aca4aee912b8f9d6421d91362cfcff03
MacSergey <macsergey@me.com>
2023-01-17 19:50:16 +0300

    Added resource utility

---
+++ ModShared/Utilities/Extensions/ResourceUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace ModsCommon.Utilities
+{
+    public static class ResourceUtility
+    {
+        public static byte[] LoadResource(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var manifestResourceStream = assembly.GetManifestResourceStream(name);
+            var data = new byte[manifestResourceStream.Length];
+            manifestResourceStream.Read(data, 0, data.Length);
+            return data;
+        }
+    }
+}
