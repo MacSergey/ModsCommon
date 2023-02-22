3199b2c4b8819b99920aa9bda6fd422fc2700dcb
MacSergey <macsergey@me.com>
2023-02-22 12:30:25 +0300

    Update loading extension

--- LoadExtensionShared/LoadingExtension.cs
+++ LoadExtensionShared/LoadingExtension.cs
@@ -6,6 +6,22 @@ namespace ModsCommon.Utilities
     public abstract class BaseLoadingExtension<TypeMod> : LoadingExtensionBase
         where TypeMod : BaseMod<TypeMod>
     {
+        public sealed override void OnCreated(ILoading loading)
+        {
+            SingletonMod<TypeMod>.Instance.Logger.Debug($"On create loading extension");
+            base.OnCreated(loading);
+            OnPreLoaded();
+        }
+        protected virtual void OnPreLoaded() { }
+
+        public sealed override void OnReleased()
+        {
+            SingletonMod<TypeMod>.Instance.Logger.Debug($"On release loading extension");
+            base.OnReleased();
+            OnPreUnloaded();
+        }
+        protected virtual void OnPreUnloaded() { }
+
         public sealed override void OnLevelLoaded(LoadMode mode)
         {
             SingletonMod<TypeMod>.Instance.Logger.Debug($"On level loaded");
@@ -22,16 +38,18 @@ namespace ModsCommon.Utilities
                     break;
             }
         }
-        public sealed override void OnLevelUnloading()
-        {
-            SingletonMod<TypeMod>.Instance.Logger.Debug($"On level unloading");
-            OnUnload();
-        }
         protected virtual void OnLoad()
         {
             SingletonMod<TypeMod>.Instance.ShowWhatsNew();
             SingletonMod<TypeMod>.Instance.ShowBetaWarning();
         }
+
+        public sealed override void OnLevelUnloading()
+        {
+            SingletonMod<TypeMod>.Instance.Logger.Debug($"On level unloading");
+            OnUnload();
+        }
         protected virtual void OnUnload() { }
+
     }
 }
