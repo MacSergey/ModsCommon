57206704fc7e5d5e216deecd9b55a1deee4d61fc
MacSergey <macsergey@me.com>
2023-04-02 17:41:54 +0300

    remove IMT code from common

--- PropertyValueShared/PropertyValue.cs
+++ PropertyValueShared/PropertyValue.cs
@@ -1,5 +1,4 @@
-﻿using IMT.Utilities;
-using System;
+﻿using System;
 using System.Xml.Linq;
 using UnityEngine;
 
@@ -345,38 +344,6 @@ namespace ModsCommon.Utilities
         {
             RawName = config.GetAttrValue(Label, string.Empty);
             Value = PrefabCollection<PrefabType>.FindLoaded(RawName) ?? defaultValue;
-        }
-    }
-
-    public class PropertyThemeValue : PropertyClassValue<ThemeHelper.IThemeData>
-    {
-        public string RawName { get; private set; } = string.Empty;
-        public bool HasName => !string.IsNullOrEmpty(RawName);
-
-        public override ThemeHelper.IThemeData Value 
-        {
-            set
-            {
-                base.Value = value;
-                RawName = value?.Id ?? string.Empty;
-            }
-        }
-
-        public PropertyThemeValue(Action onChanged, ThemeHelper.IThemeData value = default) : base(onChanged, value) { }
-        public PropertyThemeValue(string label, Action onChanged, ThemeHelper.IThemeData value = default) : base(label, onChanged, value) { }
-
-        public PropertyThemeValue(Action onChanged, string name) : this(onChanged, ThemeHelper.TryGetTheme(name, out var theme) ? theme : null) { }
-        public PropertyThemeValue(string label, Action onChanged, string name) : this(label, onChanged, ThemeHelper.TryGetTheme(name, out var theme) ? theme : null) { }
-
-        protected override void ToXml(out string label, out object value)
-        {
-            label = Label;
-            value = Value?.Id ?? RawName;
-        }
-        public override void FromXml(XElement config, ThemeHelper.IThemeData defaultValue)
-        {
-            RawName = config.GetAttrValue(Label, string.Empty);
-            Value = ThemeHelper.TryGetTheme(RawName, out var theme) ? theme : defaultValue;
         }
     }
 }
