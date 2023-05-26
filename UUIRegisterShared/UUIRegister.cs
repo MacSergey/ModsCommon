b82466c38d704c94df3d8d5aa1d577134238adb6
ssv2 <sylvie.valoria@proton.me>
2023-05-25 22:21:16 -0400

    Added UnifiedUI (UUI) 2.2.1 - Continued (WorkshopID #2966990700) to the UUI Register function

--- UUIRegisterShared/UUIRegister.cs
+++ UUIRegisterShared/UUIRegister.cs
@@ -20,7 +20,7 @@ namespace ModsCommon
     where TypeMod : ICustomMod
     where TypeTool : ToolBase, ITool
     {
-        private static PluginSearcher UUISearcher { get; } = PluginUtilities.GetSearcher("Unified UI", 2255219025ul) & new VersionSearcher(new Version(1, 3), (m, s) => m >= s);
+        private static PluginSearcher UUISearcher { get; } = PluginUtilities.GetSearcher("Unified UI", 2255219025ul, 2966990700ul) & new VersionSearcher(new Version(1, 3), (m, s) => m >= s);
         public static bool IsUUIEnabled => UUISearcher.GetPlugin()?.isEnabled == true;
 
         public bool UUIRegistered { get; private set; }
